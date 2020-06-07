5f0d0134e03dac0531222335fa1f5914dbe3c49c
zhengzhaoxiang <1059311525@qq.com>
2020-06-07 21:59:58 +0800

    进度：6. Ribbon 完成

---
+++ microservicecloud-provider-dept-8002/src/main/java/com.agg/springcloud/service/DeptService.java
@@ -0,0 +1,14 @@
+package com.agg.springcloud.service;
+
+import com.agg.springcloud.entities.Dept;
+
+import java.util.List;
+
+public interface DeptService {
+
+    public boolean add(Dept dept);
+
+    public Dept get(Long id);
+
+    public List<Dept> list();
+}
---
+++ microservicecloud-provider-dept-8002/src/main/java/com.agg/springcloud/service/serviceImpl/DeptServiceImpl.java
@@ -0,0 +1,31 @@
+package com.agg.springcloud.service.serviceImpl;
+
+import com.agg.springcloud.dao.DeptDao;
+import com.agg.springcloud.entities.Dept;
+import com.agg.springcloud.service.DeptService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DeptServiceImpl implements DeptService {
+    @Autowired
+    private DeptDao dao;
+
+    @Override
+    public boolean add(Dept dept) {
+        return dao.addDept(dept);
+    }
+
+    @Override
+    public Dept get(Long id) {
+        return dao.findById(id);
+    }
+
+    @Override
+    public List<Dept> list() {
+        return dao.findAll();
+    }
+
+}
