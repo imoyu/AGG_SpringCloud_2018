1fcc0683790401fed60f186065e6e5a6ef07e2ba
zhengzhaoxiang <1059311525@qq.com>
2020-06-08 16:13:04 +0800

    进度 全部 完成

---
+++ microservicecloud-config-dept-client-8001/src/main/java/com/agg/springcloud/dao/DeptDao.java
@@ -0,0 +1,15 @@
+package com.agg.springcloud.dao;
+
+import com.agg.springcloud.entities.Dept;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface DeptDao {
+    public boolean addDept(Dept dept);
+
+    public Dept findById(Long id);
+
+    public List<Dept> findAll();
+}
