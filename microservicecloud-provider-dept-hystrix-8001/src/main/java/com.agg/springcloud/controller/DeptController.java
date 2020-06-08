da1c0e886a64af676551dc1fe391d2ad4497b2d8
zhengzhaoxiang <1059311525@qq.com>
2020-06-08 10:23:23 +0800

    进度：8. Hystrix 完成

---
+++ microservicecloud-provider-dept-hystrix-8001/src/main/java/com.agg/springcloud/controller/DeptController.java
@@ -0,0 +1,34 @@
+package com.agg.springcloud.controller;
+
+import com.agg.springcloud.entities.Dept;
+import com.agg.springcloud.service.DeptService;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DeptController {
+    @Autowired
+    private DeptService service;
+
+    @RequestMapping(value = "/dept/get/{id}", method = RequestMethod.GET)
+    @HystrixCommand(fallbackMethod = "processHystrix_Get")
+    public Dept get(@PathVariable("id") Long id) {
+        Dept dept = this.service.get(id);
+        if (dept == null) {
+            throw new RuntimeException("该ID：" + id + "没有没有对应的信息");
+        }
+        return dept;
+    }
+
+    // 调用服务方法失败并抛出了错误信息后，执行 fallbackMethod
+    public Dept processHystrix_Get(@PathVariable("id") Long id) {
+        return new Dept().setDeptno(id)
+                .setDname("该ID：" + id + "没有没有对应的信息,null--@HystrixCommand")
+                .setDb_source("no this database in MySQL");
+    }
+
+}
